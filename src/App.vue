4f671a0a35a58ce22f828b30c379761fb5224863
rakhzar <mnv36ru@gmail.com>
2026-03-20 21:25:00 +0300

    feat: add App.vue layout with Sidebar and basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
-<template></template>
+<template>
+  <div class="app">
+    <Sidebar />
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import Sidebar from './components/Sidebar.vue';
+</script>
+
+<style>
+.app {
+  display: flex;
+}
+
+.content {
+  min-width: 900px;
+  padding: 45px;
+}
+
+main {
+  margin-top: 30px;
+}
+</style>
