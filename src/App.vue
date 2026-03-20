1351685261e6f0009023a9a13b4327859dd7661c
rakhzar <mnv36ru@gmail.com>
2026-03-20 23:27:58 +0300

    feat: integrate AddHabitModal component into App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="app">
     <Sidebar />
+    <AddHabitModal />
   </div>
 </template>
 
 <script setup>
 import Sidebar from './components/Sidebar.vue';
+import AddHabitModal from './components/modals/AddHabitModal.vue';
 </script>
 
 <style>
