1b99afe56ecc6a1cf5198f43df9dcdc770ceb9e3
rakhzar <mnv36ru@gmail.com>
2026-03-21 13:59:29 +0300

    refactor: update App layout, remove AddDayForm, add DaysList component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="app">
-    <Sidebar />
-    <div class="content">
-      <HeaderStats />
-      <main></main>
-    </div>
-    <AddHabitModal />
+  <Sidebar />
+  <div class="content">
+    <HeaderStats />
+    <main>
+      <DaysList />
+    </main>
   </div>
+  <AddHabitModal />
 </template>
 
 <script setup>
 import Sidebar from './components/Sidebar.vue';
 import AddHabitModal from './components/AddHabitModal.vue';
 import HeaderStats from './components/HeaderStats.vue';
+import DaysList from './components/DaysList.vue';
+import AddDayForm from './components/AddDayForm.vue';
 </script>
 
 <style>
-.app {
+#app {
   display: flex;
 }
 
